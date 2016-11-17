dc56d1af0d194a5c1f715c7260d7aaa6c9fe792f
Yunhoo-Kim <zkfltm3896@gmail.com>
2016-11-17 10:58:25 +0900

    source file

---
+++ 20161117ComputerPractice/20161117ComputerPractice/thread.cpp
@@ -0,0 +1,68 @@
+#include <process.h>
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+class KeyEvent {
+private:
+	HANDLE hln;
+	COORD KeyWhere;
+	DWORD EventCount;
+	INPUT_RECORD InRec;
+	DWORD NumRead;
+	int downKey;
+public:
+	KeyEvent() {
+		hln = GetStdHandle(STD_INPUT_HANDLE);
+		EventCount = 1;
+	}
+	int getkey() {
+		ReadConsoleInput(hln, &InRec, 1, &NumRead);
+		if (InRec.EventType == KEY_EVENT) {
+			if (InRec.Event.KeyEvent.bKeyDown) {
+				downKey = InRec.Event.KeyEvent.wVirtualKeyCode;
+				return downKey;
+			}
+			else {
+				return -1;
+			}
+		}
+		return -1;
+	}
+};
+unsigned int __stdcall mythread(void*) {
+	int i = 0;
+	while (1) {
+		cout << GetCurrentThreadId() << ": " << i++ << endl;
+		Sleep(500);
+	}
+	return 0;
+}
+unsigned int __stdcall keyEvent(void *) {
+	KeyEvent k;
+	int i;
+	while (1) {
+		i = k.getkey();
+		switch (i)
+		{
+		case 37: cout << "Left key is pressed" << endl; break;
+		case 38: cout << "Left key is pressed" << endl; break;
+		case 39: cout << "Left key is pressed" << endl; break;
+		case 40: cout << "Left key is pressed" << endl; break;
+		
+		}
+	}
+	return 0;
+}
+
+int main() {
+	HANDLE handleA, handleB;
+	handleA = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
+	handleB = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
+
+	WaitForSingleObject(handleA, INFINITE);
+	WaitForSingleObject(handleB, INFINITE);
+	
+	CloseHandle(handleA);
+}
