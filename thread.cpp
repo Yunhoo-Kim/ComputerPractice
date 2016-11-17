2004afc4d6bdd07270d7e7534622122a08c1cf2a
Yunhoo <Yunhoo>
2016-11-18 01:26:28 +0900

    submit project

--- thread.cpp
+++ thread.cpp
@@ -1,36 +1,7 @@
-#include <process.h>
-#include <iostream>
-#include <windows.h>
+#include "KeyEvent.h"
 
 using namespace std;
 
-class KeyEvent {
-private:
-	HANDLE hln;
-	COORD KeyWhere;
-	DWORD EventCount;
-	INPUT_RECORD InRec;
-	DWORD NumRead;
-	int downKey;
-public:
-	KeyEvent() {
-		hln = GetStdHandle(STD_INPUT_HANDLE);
-		EventCount = 1;
-	}
-	int getkey() {
-		ReadConsoleInput(hln, &InRec, 1, &NumRead);
-		if (InRec.EventType == KEY_EVENT) {
-			if (InRec.Event.KeyEvent.bKeyDown) {
-				downKey = InRec.Event.KeyEvent.wVirtualKeyCode;
-				return downKey;
-			}
-			else {
-				return -1;
-			}
-		}
-		return -1;
-	}
-};
 unsigned int __stdcall mythread(void*) {
 	int i = 0;
 	while (1) {
